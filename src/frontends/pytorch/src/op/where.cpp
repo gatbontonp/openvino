9cd40a1001160b30c79fc0ad2e96162990ae1518
Philip Gatbonton <gatbontonpc@gmail.com>
2025-01-08 18:12:35 -0800

    add where op
    Signed-off-by: Philip Gatbonton <gatbontonpc@gmail.com>

--- src/frontends/pytorch/src/op/where.cpp
+++ src/frontends/pytorch/src/op/where.cpp
@@ -4,7 +4,9 @@
 
 #include "openvino/frontend/pytorch/node_context.hpp"
 #include "openvino/op/convert.hpp"
+#include "openvino/op/non_zero.hpp"
 #include "openvino/op/select.hpp"
+#include "openvino/op/transpose.hpp"
 #include "utils.hpp"
 
 namespace ov {
@@ -16,8 +18,19 @@ using namespace ov::op;
 
 OutputVector translate_where(const NodeContext& context) {
     num_inputs_check(context, 1, 3);
+    //  atten::where(condition, input, other, *, out=None) → Tensor
+
     auto cond = context.get_input(0);
-    PYTORCH_OP_CONVERSION_CHECK(!context.input_is_none(1), "aten::where(cond) unsupported");
+    if (context.get_input_size() == 1) {
+
+        // aten::where(condition) is identical to torch.nonzero(condition, as_tuple=True)
+        auto non_zero = context.mark_node(std::make_shared<v3::NonZero>(cond));
+        auto input_order = context.mark_node(v0::Constant::create(element::i32, Shape{2}, {1, 0}));
+
+        return {context.mark_node(std::make_shared<v1::Transpose>(non_zero, input_order))};
+    }
+    
+    // PYTORCH_OP_CONVERSION_CHECK(!context.input_is_none(1), "aten::where(cond) unsupported");
     auto bool_cond = context.mark_node(std::make_shared<v0::Convert>(cond, element::boolean));
     Output<Node> x;
     Output<Node> y;
